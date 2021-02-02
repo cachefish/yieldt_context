a75ad625625e8dcda94c7b5657278f132f7bcc8d
cachefish <780207419@qq.com>
2021-02-02 16:42:53 +0800

    update

--- yieldt.h
+++ yieldt.h
@@ -7,7 +7,7 @@
 #define DEFAULT_STACK_SIZE (1024*128)
 #define MAX_YEILD_SIZE  1024
 
-enum ThreadState{FREE,RUNNABLE,RUNNING,SUSPEND};
+enum ThreadState{FREE,RUNNABLE,RUNNING,SUSPEND};  //空闲，就绪，正在执行和挂起
 
 /*
 typedef struct ucontext {  
@@ -19,19 +19,19 @@ typedef struct ucontext {
 } ucontext_t; 
 */
 
-
-
 struct schedule_t;
 
 typedef void (*Fun)(void *arg);
 
 typedef struct yieldt_t
 {
-    ucontext_t ctx;
-    Fun func;
+    ucontext_t ctx;  //ctx保存协程的上下文
+
+    Fun func;       //用户函数
     void *arg;
-    enum ThreadState state;     //该线程状态
-    char stack[DEFAULT_STACK_SIZE]; 
+
+    enum ThreadState state;     //该线程运行状态
+    char stack[DEFAULT_STACK_SIZE];  //协程栈
 }yeildt_t;
 
 typedef struct schedule_t
@@ -60,10 +60,26 @@ static void yieldt_body(schedule_t *ps);
 //创建用户线程  返回计划中已创建线程的索引
 int yieldt_create(schedule_t &schedule,Fun func,void *arg);
 
-/*挂起当前正在运行的线程，切换到主线程*/
+/*挂起当前正在运行的线程，切换到主线程
+
+                        挂起当前正在运行的协程。首先是将running_thread置为-1，
+                        将正在运行的协程的状态置为SUSPEND，最后切换到主函数上下文。
+
+*/
 void yieldt_yield(schedule_t &schedule);
-/*恢复索引等于id的线程*/
+
+
+
+/*恢复索引等于id的线程
+                        如果指定的协程是首次运行，
+                        处于RUNNABLE状态，则创建一个上下文，
+                        然后切换到该上下文。如果指定的协程已经运行过，
+                        处于SUSPEND状态，则直接切换到该上下文即可
+*/
 void yieldt_resume(schedule_t &schedule,int id);
+
+
+
 /*测试调度中的所有线程是否都结束*/
 int schedule_finished(const schedule_t &schedule);
 
